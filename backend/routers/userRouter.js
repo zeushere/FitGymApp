596532cb657d04c571b1c2d14c2665c38c4f2b04
ZEUSHEREPC\zeushere <kacper.roda37@gmail.com>
2022-01-30 00:04:47 +0100

    missing import in userRouter

--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import expressAsyncHandler from 'express-async-handler';
 import bcrypt from 'bcryptjs';
+import data from '../data.js';
 import User from '../models/userModel.js';
 import { generateToken, isAdmin, isAuth } from '../utils.js';
 
